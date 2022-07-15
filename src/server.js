3d0aefd79745bf635cb92412892f9796028548b7
khanhdev <nguyenphiikhanh@gmail.com>
2022-07-15 14:46:07 +0700

    mew commit 01

--- src/server.js
+++ src/server.js
@@ -5,9 +5,12 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 viewEngineConfig(app);
-app.get('/',(req, res)=>{ 
+app.get('/',(req, res)=>{
     res.render('index.ejs');
 });
 app.listen(port,()=>{
     console.log(`Express app is running in port ${port}`);
 });
+
+
+// This line to test merge and rebase
