531d6373c5b2939bb151d0a28edc048ce28ba05a
khanhdev <nguyenphiikhanh@gmail.com>
2022-07-15 13:45:21 +0700

    #base_mvc refactor: connection with environment variables

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -1,12 +1,17 @@
 // get the client
 import mysql2 from 'mysql2';
+require('dotenv').config()
 
+const HOST = process.env.DB_HOST;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASS = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_DATABASENAME;
 // create the connection to database
-const connection = mysql2.createConnection({
-    host: `localhost`,
-    user: `root`,
-    password: ``,
-    database: `cd_express_app`,
+const connection = mysql2.createPool({
+    host: `${HOST}`,
+    user: `${DB_USERNAME}`,
+    password: `${DB_PASS}`,
+    database: `${DB_NAME}`,
 });
 
 // simple query
